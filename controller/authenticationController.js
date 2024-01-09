fb6828af32d5f51df99d14303d0e69872338aadf
DESKTOP-AAHU0IQ\Tilo <info@tilorenner.com>
2024-01-09 18:30:58 +0100

    finish workday

--- controller/authenticationController.js
+++ controller/authenticationController.js
@@ -17,19 +17,15 @@ const AuthenticationController = {
 
         try{
             const matchingUser = await User.find({username:username}).exec()
-
-            console.log("MatchingL:" ,matchingUser.length)
             if(!matchingUser.length)
             {
-                console.log("Not Found user ", username)
-                console.log("Matching:" ,matchingUser)
-                console.log("MatchingL:" ,matchingUser.length)
+                console.log("Not Found user ", username, " Result: ", matchingUser, " Length: ",matchingUser.length)
 
                 let salt = await bcrypt.genSalt(10);
                 const hashedPassword = await bcrypt.hash(password,salt)
                 if(!hashedPassword)
                 {
-
+                    console.log("Error creating salt")
                 }
                 else
                 {
@@ -37,33 +33,58 @@ const AuthenticationController = {
                     const newUser = await User.create({
                         username:username,
                         hashedPassword:hashedPassword,
-                        role:"User"
-    
+                        role:"user"
                     })
+                    res.status(201).json({userCreated: true})
                 }
             }
             else
             {
-                console.log("Matching:" ,matchingUser)
-                console.log("MatchingL:" ,matchingUser.length)
-                console.log("User found ", username)
-
+                console.log("Found user ", username, " Result: ", matchingUser, " Length: ",matchingUser.length)
+                res.status(409).json({userCreated: false, message:"User already exists"})
+                
             }
-
-
-
-
-
         }
         catch(err)
         {
 
         }
 
+    },
+    login : async function(req,res)
+    {
+        console.log("Called Login")
+
+        const {username,password} = req.body
+
+        try{
+            const matchingUser = await User.findOne({username:username}).exec()
+            if(!matchingUser)
+            {
+                res.status(404).json({userLoggedIn:false, message:"user does not exist or password incorrect"})
+            }
+            else
+            {
+                const hashedPasswordFromDB = matchingUser.hashedPassword;
+                const match = await bcrypt.compare(password,matchingUser.hashedPassword)
+                console.log("Result",  match)
+                if(match)
+                {
+                    //Todo Send JWT or session
+                    res.status(200).json({message: "User logged in"})
+                }
+                else
+                {
+                    res.status(404).json({userLoggedIn:false, message:"user does not exist or password incorrect"})
+                }
+            }
+
+        }
+        catch(err)
+        {
+
+        }
     }
-
-
-
 } 
 
 
